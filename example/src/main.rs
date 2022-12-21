98a4498cbbb35841859a51af5a45ea51d2b763f9
Jacob Halsey <jacob@jhalsey.com>
2022-12-21 14:10:51 +0000

    Improve example (#9)

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,3 +1,4 @@
+use actix_easy_multipart::tempfile::Tempfile;
 use actix_easy_multipart::text::Text;
 use actix_easy_multipart::MultipartForm;
 use actix_web::{post, App, HttpServer, Responder};
@@ -5,14 +6,32 @@ use actix_web::{post, App, HttpServer, Responder};
 #[derive(MultipartForm)]
 struct Upload {
     text: Text<String>,
+    number: Text<i64>,
+    file: Tempfile,
 }
 
 #[post("/")]
 async fn route(form: MultipartForm<Upload>) -> impl Responder {
-    format!("Received text={}", &*form.text)
+    let content_type = form
+        .file
+        .content_type
+        .as_ref()
+        .map(|m| m.as_ref())
+        .unwrap_or("null");
+    let file_name = form
+        .file
+        .file_name
+        .as_ref()
+        .map(|m| m.as_ref())
+        .unwrap_or("null");
+
+    format!(
+        "Received:\ntext = {}\nnumber = {}\nfile = {} bytes, content-type = {}, filename = {}",
+        &*form.text, &*form.number, form.file.size, content_type, file_name
+    )
 }
 
-/// Test using `curl http://localhost:8080/ -v -F text=HelloWorld`
+/// Test using `curl http://localhost:8080/ -F text="Hello World" -F number=23 -F file=@localfilename`
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(route))
